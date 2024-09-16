189733b0d9344353772e11eb2ea9450e6718db3a
fajarizaf <fajarizaf@gmail.com>
2024-09-16 16:05:16 +0700

    first commit

---
+++ config/app.php
@@ -0,0 +1,23 @@
+<?php
+ return array (
+  'session' => 
+  array (
+    'driver' => 'php',
+  ),
+  'email' => 
+  array (
+    'driver' => 'mail',
+  ),
+  'cache' => 
+  array (
+    'driver' => 'APCu',
+    'servers' => 
+    array (
+    ),
+    'options' => 
+    array (
+    ),
+  ),
+  'cronkey' => 'd0t0eLm3AuhfdEbhRuDKFp3hgfHSV4Kp',
+  'key' => 'XlXUjH2E2eaVob75uvZAapATO3aPewmy',
+);
---
+++ config/sites.php
@@ -0,0 +1,13 @@
+<?php
+ return array (
+  '* * *' => 
+  array (
+    'name' => 'Default',
+    'host' => '*.*.*',
+    'theme' => 'landing',
+    'state' => 'live',
+    'template' => '',
+    'id' => 1,
+    'site_id' => 1,
+  ),
+);
